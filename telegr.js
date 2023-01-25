0195b0a7a3e4a902683e35c64e4069ef87fd5b5d
mustafa <alnoorimustafa@gmail.com>
2023-01-25 17:26:03 +0300

    4th

--- telegr.js
+++ telegr.js
@@ -13,7 +13,7 @@ bot.start((ctx) =>
 					{
 						text: "اضافة مستمسك",
 						callback_data: "btn-2",
-						url: "http://65.20.143.109:5500/index.html",
+						url: `${process.env.URL}/index.html`,
 					},
 				],
 			],
@@ -23,7 +23,7 @@ bot.start((ctx) =>
 
 bot.on("callback_query", async (ctx) => {
 	if (ctx.callbackQuery.data === "yes") {
-		ctx.reply(`تواصل مع الادمن \n \n https://t.me/M19694`);
+		ctx.reply(`تواصل مع الادمن \n \n ${process.env.ADMIN}`);
 	}
 	if (ctx.callbackQuery.data === "btn-1") {
 		ctx.reply("ادخل الاسم الثلاثي لصاحب المستمسك");
@@ -43,7 +43,7 @@ bot.command("upload", async (ctx) => {
 bot.on("text", (ctx) => {
 	axios
 		.get(
-			`http://192.168.0.102:1337/api/documents?filters[name][$eq]=${ctx.message.text}`
+			`${process.env.API_URL}/api/documents?filters[name][$eq]=${ctx.message.text}`
 		)
 		.then(function (response) {
 			let res = response.data.data;
@@ -51,36 +51,23 @@ bot.on("text", (ctx) => {
 			if (response.data.data.length > 0) {
 				axios
 					.get(
-						`http://192.168.0.102:1337/api/documents/${response.data.data[0].id}?populate=*`
+						`${process.env.API_URL}/api/documents/${response.data.data[0].id}?populate=*`
 					)
 					.then(function async(response) {
 						let image_url =
 							response.data.data.attributes.images.data[0].attributes.url;
 						ctx.replyWithPhoto(
 							{
-								url: `http://192.168.0.102:1337${image_url}`,
+								url: `${process.env.API_URL}${image_url}`,
 							},
 							{
 								caption: `
 							\n
 							 اذا كان المستمسك يعود لك تواصل مع الادمن
-							\n https://t.me/M19694`,
+							\n ${process.env.ADMIN}`,
 							}
 						);
 					});
-				// .then(
-				// 	ctx.reply("هل هذا المستمسك يعود لك", {
-				// 		reply_markup: {
-				// 			inline_keyboard: [
-				// 				/* Inline buttons. 2 side-by-side */
-				// 				[
-				// 					{ text: "نعم", callback_data: "yes" },
-				// 					{ text: "كلا", callback_data: "no" },
-				// 				],
-				// 			],
-				// 		},
-				// 	})
-				// );
 			} else {
 				ctx.reply("لا يوجد");
 			}
