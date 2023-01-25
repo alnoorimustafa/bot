fcc8f0a631387ec33824b470cb79da6a50ec3b23
mustafa <alnoorimustafa@gmail.com>
2023-01-25 18:52:13 +0300

    6th

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
 		BOT_TOKEN: "5818631169:AAE-wJCsEMXw8XmjC5DXT-yWBuktpc5i_Pg",
 		URL: "http://65.20.143.109",
 		ADMIN: "https://t.me/M19694",
-		API_URL: "http://localhost:1337",
+		API_URL: "http://127.0.0.1:1337",
 		// API_URL: "http://192.168.0.102:1337",
 	},
 };
