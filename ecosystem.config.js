8e6517dca17f8fdfee9a0d6a32345a2c3ed66850
mustafa <alnoorimustafa@gmail.com>
2023-01-25 17:59:27 +0300

    5th

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,6 @@ module.exports = {
 		{
 			name: "telegraf",
 			script: "telegr.js",
-			watch: ".",
 		},
 	],
 
@@ -11,6 +10,7 @@ module.exports = {
 		BOT_TOKEN: "5818631169:AAE-wJCsEMXw8XmjC5DXT-yWBuktpc5i_Pg",
 		URL: "http://65.20.143.109",
 		ADMIN: "https://t.me/M19694",
-		API_URL: "http://192.168.0.102:1337",
+		API_URL: "http://localhost:1337",
+		// API_URL: "http://192.168.0.102:1337",
 	},
 };
